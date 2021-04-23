7bade9b7694cca72a938dd9de29bf37f621f29d7
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-23 17:08:45 -0400

    adding files

--- CoffeeAndTea/Program.cs
+++ CoffeeAndTea/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CoffeeAndTea
 {
@@ -6,7 +8,74 @@ namespace CoffeeAndTea
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
+    }
+
+    class Payment
+    {
+        private string _name;
+        private decimal _payment;
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+        private decimal Money
+        {
+            get { return this._payment;  }
+            set { this._payment = value; }
+        }
+
+        public Payment(string name, decimal payment)
+        {
+            this._name = name;
+            this._payment = payment;
+        }
+
+        public override string ToString()
+        {
+            return $"{ this._name } { this._payment }";
+        }
+    }
+
+    class Cash : Payment
+    {
+        public Cash(string name, decimal payment) :base(name, payment)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    class CreditCards : Payment
+    {
+        private string _cardNumber;
+        private string _expDate;
+        private string _securityCode;
+
+        public string SecurityCode
+        {
+            get { return _securityCode; }
+            set { _securityCode = value; }
+        }
+
+        public string ExpirationDate
+        {
+            get { return _expDate; }
+            set { _expDate = value; }
+        }
+
+        public string CardNumber
+        {
+            get { return _cardNumber; }
+            set { _cardNumber = value; }
+        }
+
     }
 }
