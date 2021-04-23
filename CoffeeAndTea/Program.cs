e06c5b4aa785b1474b43cf242a04f553dcd53ed9
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-23 12:10:32 -0400

    first commit

---
+++ CoffeeAndTea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeeAndTea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
