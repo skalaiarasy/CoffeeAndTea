baafd4b8e0af8d52c7da95e2e9b315c7e68d0a9c
AuthenticCode313 <80369849+AuthenticCode313@users.noreply.github.com>
2021-04-23 14:12:47 -0700

    First commit

---
+++ CoffeeAndTea/Drinks.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeAndTea
+{
+    class Drinks
+    {
+        private string _category;
+        private string _name;
+        private decimal _price;
+        private string _type;
+
+        public string Category
+        {
+            get { return this._category; }
+            set { this._category = value; }
+        }
+
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+
+
+        public decimal Price
+        {
+            get { return this._price; }
+            set { this._price = value; }
+        }
+
+        public string Type
+        {
+            get { return this._type; }
+            set { this._type = value; }
+        }
+
+        public Drinks(string category, string name, decimal price, string type)
+        {
+            this._category = category;
+            this._name = name;
+            this._price = price;
+            this._type = type;
+        }
+
+        public override string ToString()
+        {
+
+            return $"Category:{this._category}Name:{this._name} Price: {this._price} Type: {this._type}";
+        }
+
+
+    }
+}
--- CoffeeAndTea/Program.cs
+++ CoffeeAndTea/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace CoffeeAndTea
 {
@@ -6,7 +8,34 @@ namespace CoffeeAndTea
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Drinks> drinks = new List<Drinks>();
+            StreamReader reader = new StreamReader("StoreList.txt");
+          
+            while (true)
+            {
+                string line = reader.ReadLine();
+                if(line == null)
+                {
+                    break;
+                } 
+                else
+                {
+                    string[] splitLine = line.Split(",");
+                    string category = splitLine[0];
+                    string name = splitLine[1];
+                    decimal price = decimal.Parse(splitLine[2]);
+                    string type = splitLine[3];
+
+                    Drinks drink = new Drinks(category, name, price, type);
+                    Console.WriteLine(drink.ToString());
+                    drinks.Add(drink);
+                    
+                }
+            }
+            reader.Close();
         }
+
+
+        
     }
 }
